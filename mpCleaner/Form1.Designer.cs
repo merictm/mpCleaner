36d5ab98a3e5121bc4ba6956df5deed64a2cb753
merictm <mericktm@gmail.com>
2024-12-22 02:20:46 +0300

    Add project files.

---
+++ mpCleaner/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace mpCleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Genel Temizleme";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Log Temizleme";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Geri Dönüşüm Temizleme";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(169, 78);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 55);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Update Artıkları Temizleme";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 55);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Prefecth Temizleme";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(169, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(143, 55);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Hakkında";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 202);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(125, 16);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Melih Meriç ÖKTEM";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 230);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mpCleaner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ mpCleaner/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mpCleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string batFilePath = "genelTemizle.bat";
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = batFilePath,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                Process process = Process.Start(processInfo);
+                process.WaitForExit();
+
+                MessageBox.Show("Genel Temizleme İşlemi Tamamlandı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string batFilePath = "logTemizle.bat";
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = batFilePath,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                Process process = Process.Start(processInfo);
+                process.WaitForExit();
+
+                MessageBox.Show("Log Temizleme İşlemi Tamamlandı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string batFilePath = "geriDonusum.bat";
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = batFilePath,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                Process process = Process.Start(processInfo);
+                process.WaitForExit();
+
+                MessageBox.Show("Geri Dönüşüm Temizleme İşlemi Tamamlandı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string batFilePath = "updateTemizle.bat";
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = batFilePath,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                Process process = Process.Start(processInfo);
+                process.WaitForExit();
+
+                MessageBox.Show("Update Artıkları Temizleme İşlemi Tamamlandı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string batFilePath = "prefecthTemizle.bat";
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = batFilePath,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                Process process = Process.Start(processInfo);
+                process.WaitForExit();
+
+                MessageBox.Show("Prefecht Temizleme İşlemi Tamamlandı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CMD ile yapılabilecek temizlik işlemlerinin bir kısmı burada sunulmuştur.","mpCleaner");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            linkLabel1.Links.Add(0, linkLabel1.Text.Length, "www.instagram.com/mericktm/");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Link.LinkData as string);
+        }
+    }
+}
